e269f1f8339e457189219578e0c7182edbc6e8ec
Proloy Mishra <namitapro0987@gmail.com>
2023-06-17 19:40:37 +0530

    bugfix

--- xeroxer/src/main.rs
+++ xeroxer/src/main.rs
@@ -1,27 +1,36 @@
 fn main() {
-    let mut prog = prod(&[[false, false], [false, true]], &[true, false]);
+    let mut prog = prod(
+        &[[false, false], [false, true]],
+        &[true, false, true, false],
+    );
+
+    println!("{:?}", &prog);
+
     let mut ip = 0;
+    let n = S * 2;
+    let rlen = n + 6;
+
     let max = prog.iter().map(|i| i.0).max().unwrap_or_default();
-    while ip < prog.len() {
+
+    while ip < prog.len().min(max) {
         let (i, j) = prog[ip];
-        prog.extend_from_within(ip - i..ip);
+        prog.extend_from_within(ip.saturating_sub(i)..ip);
         ip += j + 1;
     }
 
-    if max >= prog.len() {
+    if max > ip {
         return;
     }
 
     prog.splice(0..ip - max, []);
-    while let Some(&(i, j)) = prog.get(max) {
-        prog.extend_from_within(max - i..i);
-        prog.splice(0..prog.len().max(j + 1), []);
-    }
 
-    println!("{:?}", prog);
+    while let Some(&(i, j)) = prog.get(max) {
+        prog.extend_from_within(max - i..max);
+        prog.splice(0..prog.len().min(j + 1), []);
+    }
 }
 
-const S: usize = 22;
+const S: usize = 20;
 fn prod<const N: usize>(p: &[[bool; N]], inp: &[bool]) -> Vec<(usize, usize)> {
     let n = S * N;
     let rlen = n + 6;
@@ -30,7 +39,6 @@ fn prod<const N: usize>(p: &[[bool; N]], inp: &[bool]) -> Vec<(usize, usize)> {
         rule(&mut res, r, p.len());
     }
     assert_eq!(res.len() - 1, rlen * p.len());
-    res.push((S, 0));
     for c in inp {
         bitconv(&mut res, *c, rlen, n, p.len());
     }
@@ -41,7 +49,7 @@ fn rule<const N: usize>(v: &mut Vec<(usize, usize)>, r: [bool; N], p: usize) {
     let prev = v.len();
     let n = S * N;
     let rlen = n + 6;
-    v.extend_from_slice(&[(0, rlen * (p - 1) - 2), (0, n + 2), (0, n)]);
+    v.extend_from_slice(&[(0, rlen * (p - 1) - 2), (0, n + 2), (S, n)]);
     {
         let prev = v.len();
         for b in r {
@@ -49,33 +57,31 @@ fn rule<const N: usize>(v: &mut Vec<(usize, usize)>, r: [bool; N], p: usize) {
         }
         assert_eq!(v.len() - prev, n)
     }
-    v.extend_from_slice(&[(n, 4), (n + 3, 11), (rlen * (p - 1) - 2, rlen + 17)]);
+    v.extend_from_slice(&[(n, 5), (n + 3, 10), (rlen * (p - 1) - 1, rlen + 15)]);
     assert_eq!(v.len() - prev, rlen)
 }
 
 fn bitconv(v: &mut Vec<(usize, usize)>, b: bool, rlen: usize, n: usize, p: usize) {
     v.extend_from_slice(&[
+        (S, 0),
         (0, S - 2),
-        (b as usize * (n + 6), 6),
+        (b as usize * (n + 6), 5),
         (S, 0),
-        (0, 3),
-        (S, 0),
-        (0, rlen * p + 2),
+        (0, 2),
+        (S, rlen * p + 2),
         (0, rlen * (p - 1) - 2),
-        (3, 0),
-        (6, 0),
-        (n + 15, 5),
-        (0, 3),
-        (n + 3, 11),
-        (rlen * (p - 1) - 2, rlen + 17),
-        (0, rlen * (p - 1) - 2),
-        (3, 0),
+        (2, 0),
         (5, 0),
-        (rlen * p + 17, 3),
+        (n + 14, 4),
+        (0, 2),
+        (n + 3, 10),
+        (rlen * (p - 1) - 1, rlen + 15),
+        (2, 0),
+        (4, 0),
+        (rlen * p + 15, 3),
         (0, 1),
-        (rlen * (p - 1) - 2, rlen + 17),
+        (rlen * (p - 1) - 1, rlen + 15),
         (1, 1),
         (3, 1),
-        (S, 0),
     ]);
 }
